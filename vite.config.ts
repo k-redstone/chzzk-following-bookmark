5f7377e72a5a7d4df564c89f5f49d9588f0aa930
k-redstone <che0618@naver.com>
2025-07-23 14:58:18 +0900

    chore: 압축형태로 익스텐션 제공

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,18 @@ import { crx } from '@crxjs/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import zip from 'vite-plugin-zip-pack'
 
 import manifest from './public/manifest.config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), crx(manifest)],
+  plugins: [
+    react(),
+    tailwindcss(),
+    crx(manifest),
+    zip({ outDir: 'release', outFileName: 'release.zip' }),
+  ],
 
   resolve: {
     alias: [
