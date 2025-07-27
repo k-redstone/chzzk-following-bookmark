eda84a0d865bc2b2fddaeed85e677f156aadd7fe
k-redstone <che0618@naver.com>
2025-07-27 21:43:56 +0900

    fix: 모달이 최상위로 올라오지 않는 문제 해결

--- src/constants/chzzkEl.ts
+++ src/constants/chzzkEl.ts
@@ -3,6 +3,6 @@ export const ASIDE_CONTAINER_EXPANDED_CLASS = 'aside_is_expanded__ghOLS'
 export const ASIDE_SHOW_TOOLTIP_CLASS = 'aside_show_tooltip__Ij2rV'
 export const ASIDE_CONTENT_TARGET_CLASS = 'aside_content__j2eTE'
 export const NAV_WRAPPER_CLASS = 'navigation_bar_section__hDpyD'
-
+export const HEADER_CONTAINER_CLASS = 'header_container__zSx9T'
 export const CHAT_POWER_QUERY =
   'button.live_chatting_power_button__Ov3eJ[type="button"]'
--- src/content/components/modal/CommonModal.tsx
+++ src/content/components/modal/CommonModal.tsx
@@ -1,3 +1,6 @@
+import { useEffect, useRef } from 'react'
+
+import { HEADER_CONTAINER_CLASS } from '@/constants/chzzkEl'
 interface CommonModalProps {
   handleModalClose: () => void
   children: React.ReactNode
@@ -7,12 +10,29 @@ export default function CommonModal({
   handleModalClose,
   children,
 }: CommonModalProps) {
+  const originalZIndexRef = useRef<string | null>(null)
+  useEffect(() => {
+    const header = document.querySelector(`.${HEADER_CONTAINER_CLASS}`)
+
+    if (header instanceof HTMLElement) {
+      originalZIndexRef.current = header.style.zIndex
+      header.style.setProperty('z-index', '12000', 'important')
+    }
+
+    return () => {
+      if (header instanceof HTMLElement) {
+        if (originalZIndexRef.current) {
+          header.style.setProperty('z-index', originalZIndexRef.current)
+        } else {
+          header.style.removeProperty('z-index')
+        }
+      }
+    }
+  }, [])
+
   return (
     <div
-      className="bg-background/55 fixed inset-0 flex items-center justify-center"
-      style={{
-        zIndex: 14000,
-      }}
+      className="bg-background/55 fixed inset-0 z-[20000] flex items-center justify-center"
       onClick={() => handleModalClose()}
     >
       <div onClick={(e) => e.stopPropagation()}>{children}</div>
